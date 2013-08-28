9f57f74d4e8d7b6ac570c9ebb046b3eaafbb0e69
s12chung <steve.chung7@gmail.com>
2013-08-28 16:26:45 -0400

    ready_count code

--- app/controllers/readys_controller.rb
+++ app/controllers/readys_controller.rb
@@ -1,12 +1,20 @@
 class ReadysController < ApplicationController
   READY_COUNT_KEY = 'ready_count'
+
   def new
   end
 
   def create
-    @ready_count = Rails.cache.read(READY_COUNT_KEY) || 0
-    @ready_count += 1
-    Rails.cache.write(READY_COUNT_KEY, @ready_count)
-    puts @ready_count
+    self.ready_count += 1
+    puts ready_count
+  end
+
+  protected
+  def ready_count
+    @ready_count ||= Rails.cache.read(READY_COUNT_KEY) || 0
+  end
+
+  def ready_count=(ready_count)
+    Rails.cache.write(READY_COUNT_KEY, ready_count)
   end
 end
