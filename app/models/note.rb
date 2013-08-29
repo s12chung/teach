bf3d8719887743edda094f7c96b6c0f536c9ad46
s12chung <steve.chung7@gmail.com>
2013-08-28 22:02:15 -0400

    final polish

--- app/models/note.rb
+++ app/models/note.rb
@@ -15,7 +15,9 @@ class Note
       markdown_options = %w/autolink no_intra_emphasis/
       renderer_options = %w/hard_wrap/
       markdown = Redcarpet::Markdown.new(
-          Redcarpet::Render::HTML.new(Hash[renderer_options.map {|o| [o.to_sym, true]}]),
+          Redcarpet::Render::HTML.new(Hash[renderer_options.map {|o| [o.to_sym, true]}].merge(
+                                          link_attributes: { target: "_blank" }
+                                      )),
           Hash[markdown_options.map {|o| [o.to_sym, true]}])
       markdown.render(raw).html_safe
     end
